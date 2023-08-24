e40dda444646e73e3cd5c443c75dfc9a973e3da7
kost90 <kostdonc90@gmail.com>
2023-08-24 17:36:10 +0100

    deleted mongoose from routes,controller,server

--- backend/models/conn.js
+++ backend/models/conn.js
@@ -2,20 +2,20 @@ const { Sequelize } = require("sequelize");
 const {db, user, pw, host} = require('../config')
 
 // DB Connection Configuration
-// const sequelize = new Sequelize(db, user, pw, {
-//     host: host,
-//     dialect: "postgres",
-//     dialectOptions:{
-//         ssl:true
-//     },
-//     logging: false
-// }); //MAG, already start using environment variables - don't show these values (use dotenv.config() process.env.DB_Password package) .env
-
-const sequelize = new Sequelize("itconsultations", "postgres", "postgres", {
-    host: "localhost",
+const sequelize = new Sequelize(db, user, pw, {
+    host: host,
     dialect: "postgres",
+    dialectOptions:{
+        ssl:true
+    },
     logging: false
-});
+}); //MAG, already start using environment variables - don't show these values (use dotenv.config() process.env.DB_Password package) .env
+
+// const sequelize = new Sequelize("itconsultations", "postgres", "postgres", {
+//     host: "localhost",
+//     dialect: "postgres",
+//     logging: false
+// });
 
 // sequelize.authenticate();
 
