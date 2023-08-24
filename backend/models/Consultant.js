7e113b10c521838c5c2cb73ab511822afe9825b8
kost90 <kostdonc90@gmail.com>
2023-08-24 17:33:48 +0100

    deleted mongoose from routes,controller,server

--- backend/models/Consultant.js
+++ backend/models/Consultant.js
@@ -1,38 +1,38 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
+// const mongoose = require("mongoose");
+// const { Schema } = mongoose;
 
-const ConsultantSchema = Schema({
-    username:{
-        type: String,
-        required:true
-    },
-    password:{
-        type: String,
-        required:true
-    },
-    email:{
-        type: String,
-        required: true
-    },
-    profession:{
-        type: String,
-        required: true
-    },
-    isAdmin:{
-        type: Boolean,
-        required: true
-    },
+// const ConsultantSchema = Schema({
+//     username:{
+//         type: String,
+//         required:true
+//     },
+//     password:{
+//         type: String,
+//         required:true
+//     },
+//     email:{
+//         type: String,
+//         required: true
+//     },
+//     profession:{
+//         type: String,
+//         required: true
+//     },
+//     isAdmin:{
+//         type: Boolean,
+//         required: true
+//     },
 
-    assignedServices:[{
-        type: String,
-        required: false,
-    }],
-    unavailableDates:[{
-        type: Date,
-        required: false
-    }]
-},{ timestamps: true });
+//     assignedServices:[{
+//         type: String,
+//         required: false,
+//     }],
+//     unavailableDates:[{
+//         type: Date,
+//         required: false
+//     }]
+// },{ timestamps: true });
 
-// export default mongoose.model("Consultant", ConsultantSchema);
-module.exports = mongoose.model("Consultant", ConsultantSchema);
+// // export default mongoose.model("Consultant", ConsultantSchema);
+// module.exports = mongoose.model("Consultant", ConsultantSchema);
 
