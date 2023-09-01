d630655e1c51b314ba74b06b7480ec0f4ab7cf04
kost90 <kostdonc90@gmail.com>
2023-09-01 14:32:59 +0100

    edited hamburger close when clink on link

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,14 +5,15 @@ import { FetchLoginUser, DeleteLoginUser } from "../api/LoginInfoSlice";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPeopleGroup } from "@fortawesome/free-solid-svg-icons";
-import { faUserTie } from '@fortawesome/free-solid-svg-icons';
-import { faBusinessTime, faHouse } from '@fortawesome/free-solid-svg-icons';
+import { faUserTie } from "@fortawesome/free-solid-svg-icons";
+import { faBusinessTime, faHouse } from "@fortawesome/free-solid-svg-icons";
 import Hamburger from "./hamburger/HamburgerMenu";
 import styles from "./styles/Navbar.module.css";
 
 let user = {};
 
 const Navbar = () => {
+  const scrennWidth = window.screen.width;
   const dispatch = useDispatch();
   const { LoginUser, statuslogin, errorlogin } = useSelector(
     (state) => state.logininfo
@@ -21,7 +22,12 @@ const Navbar = () => {
   const [isOpen, setsiOpen] = useState(false);
 
   const toggleHamburger = () => {
-    setsiOpen(!isOpen);
+    console.log(scrennWidth);
+    if (scrennWidth >= 768) {
+      return;
+    } else {
+      setsiOpen(!isOpen);
+    }
   };
 
   if (Array.isArray(LoginUser)) {
@@ -49,66 +55,84 @@ const Navbar = () => {
 
   return (
     <header>
-          <div className={isOpen ? styles.logo_show : styles.logo_show_black}>
-            <FontAwesomeIcon
-              icon={faPeopleGroup}
-              className={styles.logo_nav_bar}
-            />
-            <Link to="/" className={styles.routerLink_logo}>
-              Triple Consult
+      <div className={isOpen ? styles.logo_show : styles.logo_show_black}>
+        <FontAwesomeIcon icon={faPeopleGroup} className={styles.logo_nav_bar} />
+        <Link to="/" className={styles.routerLink_logo}>
+          Triple Consult
+        </Link>
+      </div>
+      <ul className={isOpen ? styles.openHamburger : styles.closeHamburger}>
+        <li>
+          <FontAwesomeIcon icon={faHouse} className={styles.icons_header} />
+          <span>
+            <Link
+              to="/"
+              className={styles.routerLink}
+              onClick={toggleHamburger}
+            >
+              Home
             </Link>
-          </div>
-          <ul className={isOpen ? styles.openHamburger : styles.closeHamburger}>
-            <li>
-              <FontAwesomeIcon icon={faHouse} className={styles.icons_header} />
-              <span>
-                <Link to="/" className={styles.routerLink} onClick={toggleHamburger}>
-                  Home
-                </Link>
-              </span>
-            </li>
+          </span>
+        </li>
 
-            <li>
-              <FontAwesomeIcon icon={faUserTie} className={styles.icons_header} />
-              <span>
-                <Link to="/experts" className={styles.routerLink} onClick={toggleHamburger}>
-                  Our Experts
-                </Link>
-              </span>
-            </li>
+        <li>
+          <FontAwesomeIcon icon={faUserTie} className={styles.icons_header} />
+          <span>
+            <Link
+              to="/experts"
+              className={styles.routerLink}
+              onClick={toggleHamburger}
+            >
+              Our Experts
+            </Link>
+          </span>
+        </li>
 
-            {user !== undefined ? (
-              <>
-                <button className={styles.button_navbar}>
-                  <Link to={`/profilepage/${user.username}`} onClick={toggleHamburger}>Profile</Link>
-                </button>
-                <button
-                  type="button"
-                  onClick={() => {
-                    HandelClick(user.id);
-                  }}
-                  className={styles.button_navbar}
-                >
-                  <Link to="/" onClick={toggleHamburger}>Logout</Link>
-                </button>
-              </>
-            ) : (
-              <>
-                <button className={styles.button_navbar}>
-                  <Link to="/register" onClick={toggleHamburger}>Register</Link>
-                </button>
-                <button className={styles.button_navbar}>
-                  <Link to="/loginpage" onClick={toggleHamburger}>Login</Link>
-                </button>
-                <button type="button" className={styles.button_navbar}>
-                  <Link to="/contactus" onClick={toggleHamburger}>Contact us</Link>
-                </button>
-              </>
-            )}
-          </ul>
-          <div className={styles.hamburger} onClick={toggleHamburger}>
-          <Hamburger isOpen={isOpen}/>
-          </div>
+        {user !== undefined ? (
+          <>
+            <button className={styles.button_navbar}>
+              <Link
+                to={`/profilepage/${user.username}`}
+                onClick={toggleHamburger}
+              >
+                Profile
+              </Link>
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                HandelClick(user.id);
+              }}
+              className={styles.button_navbar}
+            >
+              <Link to="/" onClick={toggleHamburger}>
+                Logout
+              </Link>
+            </button>
+          </>
+        ) : (
+          <>
+            <button className={styles.button_navbar}>
+              <Link to="/register" onClick={toggleHamburger}>
+                Register
+              </Link>
+            </button>
+            <button className={styles.button_navbar}>
+              <Link to="/loginpage" onClick={toggleHamburger}>
+                Login
+              </Link>
+            </button>
+            <button type="button" className={styles.button_navbar}>
+              <Link to="/contactus" onClick={toggleHamburger}>
+                Contact us
+              </Link>
+            </button>
+          </>
+        )}
+      </ul>
+      <div className={styles.hamburger} onClick={toggleHamburger}>
+        <Hamburger isOpen={isOpen} />
+      </div>
     </header>
   );
 };
